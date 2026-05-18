3ef0b0c3a82470e2f38be125f4ccf2d8234498d3
SujithReddy930 <sujithreddysanikommu336@gmail.com>
2026-05-18 13:03:29 +0530

    add transactions page route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import AddExpense from './pages/AddExpense'
 import Analytics from './pages/Analytics'
+import Transactions from './pages/Transactions'
 import OCRScanner from './pages/OCRScanner'
 import ReceiptHistory from './pages/ReceiptHistory'
 import Profile from './pages/Profile'
@@ -22,16 +23,16 @@ export default function App() {
             <Route path="/" element={<Navigate to="/dashboard" replace />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
             <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
             <Route path="/add" element={<PrivateRoute><AddExpense /></PrivateRoute>} />
             <Route path="/analytics" element={<PrivateRoute><Analytics /></PrivateRoute>} />
             <Route path="/predictions" element={<PrivateRoute><Analytics /></PrivateRoute>} />
-            <Route path="/transactions" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+            <Route path="/transactions" element={<PrivateRoute><Transactions /></PrivateRoute>} />
             <Route path="/ocr" element={<PrivateRoute><OCRScanner /></PrivateRoute>} />
             <Route path="/history" element={<PrivateRoute><ReceiptHistory /></PrivateRoute>} />
             <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
